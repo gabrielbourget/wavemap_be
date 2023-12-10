cdf191b0f6f37eb598aa3a290727d6c160e0b61f
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-12-10 11:10:15 -0500

    drizzle-zod schemas

--- src/database/schema/utils.ts
+++ src/database/schema/utils.ts
@@ -1,11 +1,15 @@
 import { relations } from "drizzle-orm";
 import { text, pgTable, uuid } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const Cities = pgTable('Cities', {
   ID: uuid('ID').primaryKey(),
   name: text('name').notNull(),
   countryID: uuid('countryID').references(() => Countries.ID)
 });
+
+export const insertCitiesSchema = createInsertSchema(Cities);
+export const selectCitiesSchema = createSelectSchema(Cities);
 
 export const citiesRelations = relations(Cities, ({ one }) => ({
   country: one(Countries, {
@@ -19,7 +23,13 @@ export const Countries = pgTable('Countries', {
   name: text('name').notNull()
 });
 
+export const insertCountriesSchema = createInsertSchema(Countries);
+export const selectCountriesSchema = createSelectSchema(Countries);
+
 export const Roles = pgTable('Roles', {
   ID: uuid('ID').primaryKey(),
   name: text('name').notNull()
 });
+
+export const insertRolesSchema = createInsertSchema(Roles);
+export const selectRolesSchema = createSelectSchema(Roles);
